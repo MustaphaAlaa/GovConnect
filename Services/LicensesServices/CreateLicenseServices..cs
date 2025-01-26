e3e71bc2a148e88bf952e6357cb0894c3e96bba8
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-01-26 04:19:13 +0200

    Refactors license DTOs and improves logging Updates the license creation service to provide more descriptive logging

--- Services/LicensesServices/CreateLicenseServices..cs
+++ Services/LicensesServices/CreateLicenseServices..cs
@@ -7,6 +7,9 @@ using Models.LicenseModels;
 
 namespace Services.LicensesServices;
 
+/// <summary>
+///  Service responsible for Creating Local Driving License. 
+/// </summary> 
 public class LocalDrivingLicenseCreatorService : ILocalDrivingLicenseCreationService
 {
     private readonly ICreateRepository<LocalDrivingLicense> _createRepository;
@@ -20,24 +23,30 @@ public class LocalDrivingLicenseCreatorService : ILocalDrivingLicenseCreationSer
         _logger = logger;
     }
 
+    /// <summary>
+    /// Create Local Driving License Method
+    /// </summary>
+    /// <param name="entity">The object of <see cref="LocalDrivingLicense"/> that contains the data to be created.</param>
+    /// <returns>A <see cref="LocalDrivingLicenseDTO"/> object representing the created local driving license.</returns>
     public async Task<LocalDrivingLicenseDTO> CreateAsync(LocalDrivingLicense entity)
     {
-        _logger.LogInformation($"{this.GetType().Name} -- CreateAsync method called");
+
+        _logger.LogInformation($"{nameof(CreateAsync)} method in {this.GetType().Name} called.");
         try
         {
-          if (entity == null)
+            if (entity == null)
             {
-                _logger.LogError("Entity is null");
+                _logger.LogError("Entity cannot be null. A valid LocalDrivingLicense object is required.");
                 throw new ArgumentNullException(nameof(entity));
             }
 
-            var result = await _createRepository.CreateAsync(entity); 
+            var result = await _createRepository.CreateAsync(entity);
             var localDrivingLicenseDTO = _mapper.Map<LocalDrivingLicenseDTO>(result);
             return localDrivingLicenseDTO;
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, ex.Message);
+           _logger.LogError(ex, $"An error occurred while creating the local driving license: {ex.Message}");
             throw;
         }
     }
